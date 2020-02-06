54038f4330d558f74780bd672fa26ab68cb171d0
Manish <van.manish@gmail.com>
2020-02-07 01:00:47 +0530

    OneToOne is always EAGER Fetch

---
+++ demo/src/test/java/com/in28minutes/jpa/hibernate/demo/StudentRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Student;
+import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
+import com.in28minutes.jpa.hibernate.demo.repository.StudentRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class StudentRepositoryTest {
+
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	StudentRepository repository;
+	
+	@Autowired
+	EntityManager em;
+	
+	@Test
+	public void getStudentAndPassportDetails() {
+		Student student = em.find(Student.class, 20001L);
+		logger.info("Student -> {}", student);
+		logger.info("Passport -> {}", student.getPassport());   // OneToOne is always EAGER Fetch
+	}
+
+
+}
