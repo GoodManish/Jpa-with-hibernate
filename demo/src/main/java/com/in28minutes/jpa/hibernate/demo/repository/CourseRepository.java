37eb25e9ddf5b7d2681c54603cf0732df26c1117
Manish <van.manish@gmail.com>
2020-02-09 22:02:23 +0530

    oneToMany bidirectional example

--- demo/src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
+++ demo/src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
@@ -9,16 +9,17 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Review;
 
 @Repository
 @Transactional
 public class CourseRepository {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	EntityManager em;
-	
+
 	public Course findById(Long id) {
 		return em.find(Course.class, id);
 	}
@@ -28,28 +29,42 @@ public class CourseRepository {
 	}
 
 	public Course save(Course course) {
-		
-		if(course.getId()==null) {
+
+		if (course.getId() == null) {
 			em.persist(course);
-		}else {
+		} else {
 			em.merge(course);
 		}
 		return course;
 	}
-	//em keep track of entities it bound to, which is in transactional environment 
+
+	// em keep track of entities it bound to, which is in transactional environment
 	public void playWithEntityManager() {
 		Course course1 = new Course("WebService in 10 steps");
 		em.persist(course1);
-		
+
 		Course course2 = findById(10001L);
 		course2.setName("JPA in 50 steps --- updated");
-		
+
 	}
 
 	public void addReviewsForCourse() {
-		//get course 10003
+		// get course 10003
 		Course course = findById(10003L);
-		logger.info("course.reviews() -> ", course.getReviews());
-		
+		logger.info("course.reviews() -> {}", course.getReviews());
+
+		// add 2 review
+		Review review1 = new Review("Great Course", "5");
+		Review review2 = new Review("Hatsoff !!!", "5");
+		course.addReview(review1);
+		review1.setCourse(course);
+
+		course.addReview(review2);
+		review2.setCourse(course);
+
+		// save to database
+		em.persist(review1);
+		em.persist(review2);
+
 	}
 }
